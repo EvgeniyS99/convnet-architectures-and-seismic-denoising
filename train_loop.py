a39843983a8cf7472f4f1e69cdd8d5c74ab4b5f5
root <estrievich99@gmail.com>
2023-02-10 07:35:21 +0000

    Add optimizer function, add max epoch accuracy

--- train_loop.py
+++ train_loop.py
@@ -1,39 +1,40 @@
 import torch
 import torch.nn as nn
 import torchvision
-from fastai.vision.all import *
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from tqdm import tqdm, tqdm_notebook
 import pickle
 
-def train(model, opt, loss_fn, epochs, train_data, val_data, name, path):
+def train(model, opt, loss_fn, lr_scheduler, epochs, train_data, val_data, name, path):
     """
     """
     losses = {'train': [], 'val': []}
     metrics = {'train': [], 'val': []}
     device = "cuda:0"
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(opt, step_size=3, gamma=0.1)
+    #lr_scheduler = torch.optim.lr_scheduler.StepLR(opt, step_size=3, gamma=0.1)
+    max_acc = 0.
     for epoch in range(epochs):
         print('Epoch {}/{}:'.format(epoch, epochs - 1))
         for phase in ['train', 'val']:
             running_acc = 0.
             running_loss = 0.
+            
             # train phase
             if phase == 'train':
                 dataloader = train_data
-                model.train()
+                model.train()  
             # eval phase    
             else:
                 dataloader = val_data
                 model.eval()
+                
             # iterate over batch    
             for X_batch, y_batch in tqdm(dataloader, desc=f'{phase} iter:'):
                 opt.zero_grad()
                 X_batch, y_batch = X_batch.to(device), y_batch.to(device)
                 with torch.set_grad_enabled(phase == 'train'):
                     y_pred = model(X_batch)
-                    #print(y_pred.shape, y_batch.shape)
                     loss_value = loss_fn(y_pred, y_batch)
                     if phase == 'train':
                         loss_value.backward()
@@ -51,10 +52,13 @@ def train(model, opt, loss_fn, epochs, train_data, val_data, name, path):
                 lr_scheduler.step()
             
             losses[phase].append(epoch_loss)
-            metrics[phase].append(epoch_acc) 
+            metrics[phase].append(epoch_acc)
+            
             # save model weights
-            model_weights = model.state_dict()
-            torch.save(model_weights, path)
+            if phase == 'val' and epoch_acc > max_acc:
+                model_weights = model.state_dict()
+                torch.save(model_weights, path)
+                max_acc = epoch_acc
         
             print('{} Loss: {:.4f}'.format(phase, epoch_loss))
             print('{} Accuracy: {:.4f}'.format(phase, epoch_acc))
@@ -67,3 +71,6 @@ def train(model, opt, loss_fn, epochs, train_data, val_data, name, path):
         pickle.dump(metrics, metrics_file)
             
     return losses, metrics
+
+def step_scheduler(opt, step_size, gamma):
+    return torch.optim.lr_scheduler.StepLR(opt, step_size=step_size, gamma=gamma)
